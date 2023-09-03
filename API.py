c6c6ac78d97c4f92f4501ad7345d2b39ebcb7d5a
BastienAmiot <139744720+BastienAmiot@users.noreply.github.com>
2023-09-04 00:12:43 +0200

    Update API.py

--- API.py
+++ API.py
@@ -18,16 +18,24 @@ data =  ZipFile("data/main_test.zip")
 
 @app.route('/', methods=['POST'])
 def predict():
-    input_data = request.json()
+    try:
+        input_data = request.json
     
-    user_id = input_data["user_id"]
     
-    user_data = data[data['SK_ID_CURR'] == user_id]
+        user_id = input_data["user_id"]
+        
+        user_data = data[data['SK_ID_CURR'] == user_id]
     
-    predictions = model.predict_proba(user_data[user_data.columns[1:]])
-    predictions = predictions[:, 0]
     
-    return jsonify(predictions.tolist())
+        predictions = model.predict_proba(user_data[user_data.columns[1:]])
+        predictions = predictions[:, 0]
+    
+    
+        return jsonify(predictions.tolist()), input_data
+    except Exception as e:
+        app.logger.debug("Ceci est un message de débogage.")
+        app.logger.error("Une erreur s'est produite : %s", str(e))
+        return jsonify({"error": str(e)})
 
 if __name__ == '__main__':
     app.run(port=int(os.environ.get("PORT", 5000)))
