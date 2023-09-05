132228302d8695f3dd7b866b48fa8112c5dff8c4
BastienAmiot <139744720+BastienAmiot@users.noreply.github.com>
2023-09-06 01:57:16 +0200

    Create test_api.py

---
+++ test_api.py
@@ -0,0 +1,12 @@
+import API
+import requests
+
+def test_api():
+    response = requests.post('https://dashboardscoringcredit-4b3cd19d3108.herokuapp.com/', json={"user_id": 456221})
+    assert response.status_code == 200
+    predictions = response.json()
+    assert isinstance(predictions, list)
+    assert len(predictions) > 0
+
+if __name__ == '__main__':
+    test_api()
